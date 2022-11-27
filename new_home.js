9137534aaa48e3ae5874920e59ef87fe7a9a6fbc
Pranali-22 <lpranali22@gmail.com>
2022-11-27 21:40:16 +0530

    Added: UC5-Display using JSON Object

--- new_home.js
+++ new_home.js
@@ -2,35 +2,39 @@ window.addEventListener('DOMContentLoaded',(event) =>{
     createInnerHtml();
 })
 
-//Template literal ES6 feature
+//uc5
 const createInnerHtml = () => {
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
                         "<th>Salary</th><th>Start Date</th><th>Actions</th>";
-    const innerHtml = `${headerHtml}
-        <tr>
-            <th></th>
-            <th>Name</th>
-            <th>Gender</th>
-            <th>Department</th>
-            <th>Salary</th>
-            <th>Start Date</th>
-            <th>Actions</th>
-        </tr>
-        <tr>
-            <td><img class="profile" alt="" src="../assets/profile-images/Ellipse -2.png"></td>
-            <td>Narayan Mahadevan</td>
-            <td>Male</td>
-            <td><div class="dept-label">HR</div>
-                <div class="dept-label">Finance</div>
-            </td>
-            <td>3000000</td>
-            <td>1 Nov 2020</td>
-            <td>
-                <img id="1" onclick="remove(this)" src="../assets/icons/delete-black-18dp.svg" alt="delete" >
-                <img id="1" onclick="update(this)" src="../assets/icons/create-black-18dp.svg" alt="edit">
-            </td>
-        </tr>   
-    `;
-    document.querySelector('#table-display').innerHTML = innerHtml;
 
+    let empPayrollData = createEmployeePayrollJSON()[0];
+    
+    let innerHtml = `${headerHtml}`;
+    let empPayrollList = createEmployeePayrollJSON();
+    for(const empPayrollData of empPayrollList){
+        innerHtml =  `${innerHtml}
+            <tr>
+                <td><img class="profile" src="${empPayrollData._profilePic}"  alt=""></td>
+                <td>${empPayrollData._name}</td>
+                <td>${empPayrollData._gender}</td>
+                <td>${getDeptHtml(empPayrollData._department)}</td>
+                <td>${empPayrollData._salary}</td>
+                <td>${empPayrollData._startDate}</td>
+                <td>
+                    <img name="${empPayrollData._id}" onclick="remove(this)" src="../assets/icons/delete-black-18dp.svg" alt="delete" >
+                    <img name="${empPayrollData._id}" src="../assets/icons/create-black-18dp.svg" alt="edit">
+                </td>
+            </tr>   
+        `;
+    }
+       
+    document.querySelector('#table-display').innerHTML = innerHtml;
+}
+
+const getDeptHtml = (deptList) => {
+let deptHtml = '';
+for(const dept of deptList){
+    deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
+}
+return deptHtml;
 }
